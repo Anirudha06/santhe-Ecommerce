362fa302abf00d47f6ab0bf131084727d901857c
Ganesh Pai H <ganeshpaih24@gmail.com>
2023-03-21 22:46:08 +0530

    updated sell page

--- src/components/sell.jsx
+++ src/components/sell.jsx
@@ -1,26 +1,85 @@
-import React from 'react'
-import { AiFillEdit } from 'react-icons/ai';
-import "./sell.css";
+import React from 'react';
+import {
+  AiFillEdit,
+  AiFillMinusSquare,
+  AiFillPlusSquare,
+} from 'react-icons/ai';
+import './sell.css';
+import wheat from '../static/wheat.jpg';
 
 const sell = () => {
   return (
     <div>
-        <div className="container">
-            <div className='left-container'>
-                <div className="farmer-name">
-                    <AiFillEdit/>
-                    <div className="name">
-                    {'Prakash Stall'}
-                    </div>
-                </div>
+      <div className="container">
+        <div className="left-container">
+          {/* User's Stall Name */}
+          <div className="farmer-name">
+            <AiFillEdit size={20} />
+            <div className="name">{'Prakash Stall'}</div>
+          </div>
+          <br />
+          <br />
 
+          {/* Items */}
+          <div className="items">
+            <img src={wheat} alt="item-no" />
+            {/* add item img here */}
+            <div className="item-content">
+              <h2>Wheat</h2>
+              <h4>₹ 150</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing
+                elit. Excepturi deleniti deserunt eaque laudantium
+                provident itaque nemo sint numquam cum perspiciatis in
+                voluptatum, quos cumque voluptatibus cupiditate, porro
+                ratione! Natus, odit!
+              </p>
             </div>
-            <div className='right-container'>
+            <div>
+              <AiFillMinusSquare color="red" />
+            </div>
+          </div>
+          <div className="items">
+            <img src={wheat} alt="item-no" />
+            {/* add item img here */}
+            <div className="item-content">
+              <h2>Wheat</h2>
+              <h4>₹ 150</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing
+                elit. Excepturi deleniti deserunt eaque laudantium
+                provident itaque nemo sint numquam cum perspiciatis in
+                voluptatum, quos cumque voluptatibus cupiditate, porro
+                ratione! Natus, odit!
+              </p>
+            </div>
+            <div>
+              <AiFillMinusSquare color="red" />
+            </div>
+          </div>
 
-            </div>
+          {/* add icon */}
+          <div style={{ textAlign: `center` }}>
+            <AiFillPlusSquare color="green" />
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default sell
+        {/* User Details */}
+        <div className="right-container">
+          <h3>Your Details</h3>
+          <img src={wheat} alt="user" />
+          <div className="farmer-name user-detail">
+            <AiFillEdit size={20} />
+            <div className="name">{'Gayhoo'}</div>
+          </div>
+          <div className="farmer-name user-detail">
+            <AiFillEdit size={20} />
+            <div className="name">{'Phone no.'}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default sell;
